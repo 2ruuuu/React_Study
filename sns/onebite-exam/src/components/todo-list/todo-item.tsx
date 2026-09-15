cfb798ae7d47b547bce5afb92837d2fdcae40004
choi-il-woo <chldlfdn0440@naver.com>
2026-09-15 13:36:44 +0900

    공부

--- sns/onebite-exam/src/components/todo-list/todo-item.tsx
+++ sns/onebite-exam/src/components/todo-list/todo-item.tsx
@@ -1,3 +1,4 @@
+import { useDeleteTodo } from "@/store/todos";
 import { Button } from "../ui/button";
 
 interface TodoItemProps {
@@ -6,10 +7,17 @@ interface TodoItemProps {
 }
 
 const TodoItem = ({ id, content }: TodoItemProps) => {
+  const deleteTodo = useDeleteTodo();
+  const handleDeleteClick = () => {
+    deleteTodo(id);
+  };
+
   return (
     <div className="flex items-center justify-between border p-2">
       {content}
-      <Button variant={"destructive"}>삭제</Button>
+      <Button variant={"destructive"} onClick={handleDeleteClick}>
+        삭제
+      </Button>
     </div>
   );
 };
